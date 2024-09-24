dc6ef561d891d73929834d9d3b02ddc6b176d6bd
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2024-09-24 15:53:35 +0100

    chore: remove unused function

--- R/utils.R
+++
@@ -1,6 +0,0 @@
-stopf <- function(fmt, ..., .use_call = TRUE, .call = sys.call(-1L)) {
-    .call <- if (isTRUE(.use_call)) .call[1L] else NULL
-    msg <- sprintf(fmt, ...)
-    err <- simpleError(msg, .call)
-    stop(err)
-}
